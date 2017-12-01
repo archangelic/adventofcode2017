2288fe7b2943a73db4fcc0676a35784187d371a4
Mallory Hancock <malloryhancock89@gmail.com>
2017-12-01 13:34:25 -0800

    Day 1

---
+++ day1.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+number = input('Enter your number: ')
+
+# Part 1
+count = 0
+digits = []
+for i in number:
+    try:
+        if i == number[count + 1]:
+            digits.append(int(i))
+    except:
+        if i == number[-1]:
+            digits.append(int(i))
+    count += 1
+
+print('\nYour captcha solution is:')
+print(sum(digits))
+
+# Part 2
+count = 0
+p2_digits = []
+halfway = len(number) // 2
+for i in number:
+    if i == number[(count - halfway)]:
+        p2_digits.append(int(i))
+    count += 1
+
+print('\nPart 2 solution:')
+print(sum(p2_digits))
