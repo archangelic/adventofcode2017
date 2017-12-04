9e55675994f9b1b9d7eb07b48d2bd7e0fe89fd8a
Mallory Hancock <malloryhancock89@gmail.com>
2017-12-04 10:53:51 -0800

    better layout for day2

--- day2.py
+++ day2.py
@@ -1,15 +1,9 @@
 #!/usr/bin/env python3
 
 with open('spreadsheet.txt') as f:
-    rows = [row.split() for row in f.readlines()]
+    spreadsheet = [sorted([int(i) for i in row.split()]) for row in f.readlines()]
 
-spreadsheet = []
-for row in rows:
-    r = sorted([int(i) for i in row])
-    spreadsheet.append(r)
-
-checks = [row[-1] - row[0] for row in spreadsheet]
-print(sum(checks))
+print(sum([row[-1] - row[0] for row in spreadsheet]))
 
 evens = []
 for row in spreadsheet:
@@ -18,5 +12,5 @@ for row in spreadsheet:
             if i % z == 0 and i != z:
                 evens.append([i, z])
 
-divs = [a // b for a,b in evens]
-print(sum(divs))
+print(sum([a // b for a,b in evens]))
+
